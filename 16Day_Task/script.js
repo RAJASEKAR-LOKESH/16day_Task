902cd5c3fea76514d4b46ca6fa1fb3a532ff25de
RAJASEKAR L <84315997+RAJASEKAR-LOKESH@users.noreply.github.com>
2024-06-27 21:57:51 +0530

    Add files via upload

---
+++ 16Day_Task/script.js
@@ -0,0 +1,44 @@
+function displayMessage(message) {
+    if(message=="Happy Independence Day"){
+        document.getElementById('display').style.width="70%";
+        document.getElementById('display').style.marginLeft="15%";
+        document.getElementById('display').style.padding="50px";
+    }
+    document.getElementById('display').innerText = message;
+    
+    
+}
+
+setTimeout(() => {
+    displayMessage(10);
+    setTimeout(() => {
+        displayMessage(9);
+        setTimeout(() => {
+            displayMessage(8);
+            setTimeout(() => {
+                displayMessage(7);
+                setTimeout(() => {
+                    displayMessage(6);
+                    setTimeout(() => {
+                        displayMessage(5);
+                        setTimeout(() => {
+                            displayMessage(4);
+                            setTimeout(() => {
+                                displayMessage(3);
+                                setTimeout(() => {
+                                    displayMessage(2);
+                                    setTimeout(() => {
+                                        displayMessage(1);
+                                        setTimeout(() => {
+                                            displayMessage('Happy Independence Day');
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
